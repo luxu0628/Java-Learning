8f81f0d64628d9120b12a1c8665c453a66084479
Candy <2680532813@qq.com>
2025-08-26 12:37:57 +0800

    modify

--- src/main/classLearning/Demo1.java
+++ src/main/classLearning/Demo1.java
@@ -1,6 +1,6 @@
 package main.classLearning;
 
-public class demo1 {
+public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hello");
     }
---
+++ src/main/work/LlmCreatingCode.java
@@ -0,0 +1,5 @@
+package main.work;
+
+public class LlmCreatingCode {
+
+}
